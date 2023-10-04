84c0b716242d1a990d9c9f76b01e9794ae28f5fc
hihiyoyooo <rlagusxo7979@naver.com>
2023-10-04 20:57:08 +0900

    Home페이지 html,css 구현

---
+++ WordSearch/src/App.svelte
@@ -0,0 +1,10 @@
+<script>
+  import Routes from "svelte-spa-router";
+  import Home from "./pages/Home.svelte";
+
+  const routes = {
+    "/": Home,
+  };
+</script>
+
+<Routes {routes} />
---
+++ WordSearch/src/pages/Home.svelte
@@ -0,0 +1,261 @@
+<script>
+</script>
+
+<body>
+  <header>
+    <div class="top-title">WordSearch</div>
+    <div class="top-icons">
+      <div class="top-img_pen">
+        <img src="public/pen.svg" alt="" />
+        word Search Maker
+      </div>
+      <div class="top-img_home">
+        <img src="public/home.svg" alt="" />
+        More Puzzles
+      </div>
+      <div class="top-search">
+        <img src="public/search.svg" alt="" />
+        Search
+      </div>
+    </div>
+  </header>
+  <main>
+    <div class="main-content">
+      <div class="main-img_game">
+        <img src="public/game.png" alt="" />
+        <div class="img-text">Thousands ready to play online</div>
+      </div>
+      <div class="text">
+        <h1>Word Search</h1>
+        We have the best collection of word search puzzles online, with new ones
+        being added regularly.<br /> <br />They are fun to play, but also
+        educational, in fact, many teachers make use of them. <br /><br
+        />Puzzles are 100% free to play and work on desktop pc, mac, mobile and
+        tablet. Or you can go old school and print them to enjoy offline later.
+        <br /><br />
+        Plus, if you're feeling a little more adventurous, why not create your very
+        own with our simple to use Word Search Maker, and then share them with your
+        friends.
+        <br /> <br />To get started playing, just select a game from below. Best
+        of luck.
+      </div>
+    </div>
+    <div class="main-inpo">
+      <div class="main-inpo_box">
+        <h3>Television Shows</h3>
+        ∙ The Simpsons Csharacters<br /><br />
+        ∙ Family Guy Characters <br /><br />
+        ∙ Full House<br /><br />
+        ∙ Grey's Anatomy <br /><br />
+        ∙ Star Trek <br /><br />
+        ∙ Bones
+      </div>
+      <div class="main-inpo_box">
+        <h3>Television Shows</h3>
+        ∙ The Simpsons Csharacters<br /><br />
+        ∙ Family Guy Characters <br /><br />
+        ∙ Full House<br /><br />
+        ∙ Grey's Anatomy <br /><br />
+        ∙ Star Trek <br /><br />
+        ∙ Bones
+      </div>
+      <div class="main-inpo_box">
+        <h3>Television Shows</h3>
+        ∙ The Simpsons Csharacters<br /><br />
+        ∙ Family Guy Characters <br /><br />
+        ∙ Full House<br /><br />
+        ∙ Grey's Anatomy <br /><br />
+        ∙ Star Trek <br /><br />
+        ∙ Bones
+      </div>
+      <div class="main-inpo_box">
+        <h3>Television Shows</h3>
+        ∙ The Simpsons Csharacters<br /><br />
+        ∙ Family Guy Characters <br /><br />
+        ∙ Full House<br /><br />
+        ∙ Grey's Anatomy <br /><br />
+        ∙ Star Trek <br /><br />
+        ∙ Bones
+      </div>
+
+      <div class="main-inpo_box">
+        <h3>Television Shows</h3>
+        ∙ The Simpsons Csharacters<br /><br />
+        ∙ Family Guy Characters <br /><br />
+        ∙ Full House<br /><br />
+        ∙ Grey's Anatomy <br /><br />
+        ∙ Star Trek <br /><br />
+        ∙ Bones
+      </div>
+      <div class="main-inpo_box">
+        <h3>Television Shows</h3>
+        ∙ The Simpsons Csharacters<br /><br />
+        ∙ Family Guy Characters <br /><br />
+        ∙ Full House<br /><br />
+        ∙ Grey's Anatomy <br /><br />
+        ∙ Star Trek <br /><br />
+        ∙ Bones
+      </div>
+      <div class="main-inpo_box">
+        <h3>Television Shows</h3>
+        ∙ The Simpsons Csharacters<br /><br />
+        ∙ Family Guy Characters <br /><br />
+        ∙ Full House<br /><br />
+        ∙ Grey's Anatomy <br /><br />
+        ∙ Star Trek <br /><br />
+        ∙ Bones
+      </div>
+      <div class="main-inpo_box">
+        <h3>Television Shows</h3>
+        ∙ The Simpsons Csharacters<br /><br />
+        ∙ Family Guy Characters <br /><br />
+        ∙ Full House<br /><br />
+        ∙ Grey's Anatomy <br /><br />
+        ∙ Star Trek <br /><br />
+        ∙ Bones
+      </div>
+
+      <div class="main-inpo_box">
+        <h3>Television Shows</h3>
+        ∙ The Simpsons Csharacters<br /><br />
+        ∙ Family Guy Characters <br /><br />
+        ∙ Full House<br /><br />
+        ∙ Grey's Anatomy <br /><br />
+        ∙ Star Trek <br /><br />
+        ∙ Bones
+      </div>
+      <div class="main-inpo_box">
+        <h3>Television Shows</h3>
+        ∙ The Simpsons Csharacters<br /><br />
+        ∙ Family Guy Characters <br /><br />
+        ∙ Full House<br /><br />
+        ∙ Grey's Anatomy <br /><br />
+        ∙ Star Trek <br /><br />
+        ∙ Bones
+      </div>
+      <div class="main-inpo_box">
+        <h3>Television Shows</h3>
+        ∙ The Simpsons Csharacters<br /><br />
+        ∙ Family Guy Characters <br /><br />
+        ∙ Full House<br /><br />
+        ∙ Grey's Anatomy <br /><br />
+        ∙ Star Trek <br /><br />
+        ∙ Bones
+      </div>
+      <div class="main-inpo_box">
+        <h3>Television Shows</h3>
+        ∙ The Simpsons Csharacters<br /><br />
+        ∙ Family Guy Characters <br /><br />
+        ∙ Full House<br /><br />
+        ∙ Grey's Anatomy <br /><br />
+        ∙ Star Trek <br /><br />
+        ∙ Bones
+      </div>
+    </div>
+  </main>
+  <footer>
+    <div>
+      <span>Data Consent Preferences</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      <span>Site Map</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      <span>Privacy Policy</span>&nbsp;&nbsp;&nbsp;
+      <span>TheWordSearch.com</span>
+    </div>
+    <div>
+      <span>Printable Word Searches</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      <span>Play Hangman</span>
+    </div>
+  </footer>
+</body>
+
+<style>
+  body {
+    background-color: #faffe4;
+  }
+  header {
+    border-bottom: 5px solid #028080;
+  }
+
+  main {
+    margin-top: 25px;
+  }
+  .top-title {
+    position: absolute;
+    font-size: 50px;
+    color: #028080;
+  }
+
+  .top-icons {
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+  }
+
+  .top-icons img {
+    width: 20px;
+    height: 20px;
+  }
+
+  .top-icons div {
+    margin-top: 20px;
+    margin-right: 20px;
+  }
+
+  .main-content {
+    display: flex;
+  }
+
+  .main-img_game {
+    background-color: #028080;
+    width: 570px;
+    height: 430px;
+    position: relative;
+  }
+
+  .main-img_game img {
+    position: absolute;
+    top: 20px;
+    left: 15px;
+  }
+
+  .img-text {
+    position: absolute;
+    top: 380px;
+    left: 180px;
+    color: white;
+    font-weight: bold;
+  }
+  .text {
+    width: 1300px;
+    padding-top: 10px;
+    padding-left: 30px;
+    font-size: 17px;
+    border: 1px solid #8ac34a;
+  }
+
+  .main-inpo {
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    flex-flow: row wrap;
+    border: 1px solid #8ac34a;
+  }
+  .main-inpo_box {
+    width: 400px;
+    height: 300px;
+    border: 1px solid #028080;
+    margin-top: 20px;
+    padding-left: 10px;
+    font-size: 15px;
+  }
+  footer {
+    width: 1875px;
+    height: 100px;
+    flex-direction: column;
+    padding-top: 25px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: white;
+    background-color: #555555;
+  }
+</style>
